9a8596859f38c1eed0388d011c94eb3229734bc8
Valerie Cayo <vc74377n@pace.edu>
2015-01-31 11:14:47 -0500

    Initial commit from local git

---
+++ Matchismo/CardGameAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Matchismo
+//
+//  Created by Valerie Cayo on 1/31/15.
+//  Copyright (c) 2015 Valerie Cayo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Matchismo/CardGameViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Matchismo
+//
+//  Created by Valerie Cayo on 1/31/15.
+//  Copyright (c) 2015 Valerie Cayo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
