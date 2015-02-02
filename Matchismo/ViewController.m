7c78bf9b16042815c991366c1c6fb8079bb96718
Valerie Cayo <vc74377n@pace.edu>
2015-02-01 22:03:17 -0500

    Created view and began hooking up the view to the View Controller.

---
+++ Matchismo/ViewController.m
@@ -0,0 +1,20 @@
+//
+//  ViewController.m
+//  Matchismo
+//
+//  Created by Valerie Cayo on 1/31/15.
+//  Copyright (c) 2015 Valerie Cayo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (IBAction)touchCardButton:(UIButton *)sender {
+}
+
+@end
