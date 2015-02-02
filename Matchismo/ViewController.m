94ddea8d0057a6bcb8722da3adbc1ed87405e15e
Valerie Cayo <vc74377n@pace.edu>
2015-02-01 23:06:47 -0500

    Created a label that updates the number of times the card is flipped.

--- Matchismo/ViewController.m
+++ Matchismo/ViewController.m
@@ -9,10 +9,17 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *flipsLabel;
+@property (nonatomic) int flipCount;
 
 @end
 
 @implementation ViewController
+
+-(void)setFlipCount:(int)flipCount {
+    _flipCount= flipCount;
+    self.flipsLabel.text = [NSString stringWithFormat:@"Flips: %d", self.flipCount];
+}
 
 - (IBAction)touchCardButton:(UIButton *)sender {
     
@@ -25,6 +32,8 @@
                           forState:UIControlStateNormal];
         [sender setTitle:@"A♣︎" forState:UIControlStateNormal];
     }
+    
+    self.flipCount++;
 }
 
 @end
